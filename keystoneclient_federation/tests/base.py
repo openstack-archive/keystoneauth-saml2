393259375228cd1f9ad909740590e15691109acb
Jamie Lennox <jamielennox@redhat.com>
2015-01-28 09:49:07 +1000

    Copy the existing federation plugins over.
    Take as close to a direct copy of existing plugins as we can from the
    existing keystoneclient directory.
    
    Closes-Bug: #1420118
    Change-Id: Iae1da0f66532a82e4998b63f37ef29c858d2452c

--- keystoneclient_federation/tests/base.py
+++ keystoneclient_federation/tests/base.py
@@ -15,9 +15,37 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import uuid
+
+from keystoneclient import auth
+from keystoneclient import session
+from oslo.config import fixture as config
 from oslotest import base
+from requests_mock.contrib import fixture as requests_mock
 
 
 class TestCase(base.BaseTestCase):
 
     """Test case base class for all unit tests."""
+
+    TEST_ROOT_URL = 'http://127.0.0.1:5000/'
+    TEST_URL = '%s%s' % (TEST_ROOT_URL, 'v3')
+
+    TEST_USER = uuid.uuid4().hex
+    TEST_USER_ID = uuid.uuid4().hex
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.conf_fixture = self.useFixture(config.Config())
+
+        self.requests = self.useFixture(requests_mock.Fixture())
+        self.session = session.Session()
+
+    def register_conf_options(self, group=None, section=None):
+        auth.register_conf_options(self.conf_fixture.conf, group=group)
+
+        if section:
+            self.conf_fixture.config(group=group, auth_section=section)
+            auth.register_conf_options(self.conf_fixture.conf, group=group)
+
+        return self.conf_fixture
